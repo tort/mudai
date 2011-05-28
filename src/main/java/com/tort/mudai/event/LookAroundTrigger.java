4fa33fe8b6196066476e530d5139c8f30739c6cc
tort <langeran@gmail.com>
2011-05-28 16:15:48 +0400

    make look around trigger deal with \r properly

--- src/main/java/com/tort/mudai/event/LookAroundTrigger.java
+++ src/main/java/com/tort/mudai/event/LookAroundTrigger.java
@@ -10,7 +10,10 @@ import java.util.regex.Pattern;
 
 public class LookAroundTrigger implements EventTrigger {
     public static final Pattern PATTERN = PatternUtil.compile("^(?:Вы поплелись на (?:север|юг|запад|восток)\\.\n)?" +
-            "\u001B\\[1\\;36m(.*)\u001B\\[0\\;37m$\\s\\s\\s.*\n\n\u001B\\[1\\;33m(?:(.*)\n)?\u001B\\[1\\;31m(?:(.*)\n)?\u001B\\[0\\;37m\n\u001B\\[0\\;32m[^\n]*$");
+            "\u001B\\[1\\;36m(.*)\u001B\\[0\\;37m$\\s\\s\\s.*\r{0,}\n\r{0,}\n" +
+            "\u001B\\[1\\;33m(?:(.*)\r{0,}\n)?" +
+            "\u001B\\[1\\;31m(?:(.*)\r{0,}\n)?" +
+            "\u001B\\[0\\;37m\r{0,}\n\u001B\\[0\\;32m[^\n]*$");
 
     private final EventDistributor _eventDistributor;
 
