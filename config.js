f1e4ae20064fda8b1af46d09b073efe1b361c8a4
tort <langeran@gmail.com>
2012-07-30 23:54:34 +0400

    extract js trigger constructor

--- config.js
+++ config.js
@@ -1,5 +1,19 @@
+function trigger(regex, command) {
+    return {
+        reply: function (text) {
+            if (regex.test(text)) commandExecutor.submit(new com.tort.mudai.command.RawWriteCommand(command));
+        }
+    }
+}
+
+personNameTrigger = trigger(/^Введите имя персонажа/, "ладень");
+
 function onMudEvent(text) {
-    if ((/^Введите имя персонажа/).test(text)) commandExecutor.submit(new com.tort.mudai.command.RawWriteCommand("ладень"));
+    personNameTrigger.reply(text);
+
+    if ((/^Персонаж с таким именем уже существует. Введите пароль/).test(text))
+        commandExecutor.submit(new com.tort.mudai.command.RawWriteCommand("таганьйорк"));
 
     return text.replace("\u001B[0;32m", "\u001B[0;35m");
 }
+
