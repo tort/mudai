272c1e5e335814c66c87c2c61b9498c4592c2294
tort <langeran@gmail.com>
2013-07-26 00:46:39 +0400

    refactor person a bit

--- src/main/scala/com/tort/mudai/person/Person.scala
+++ src/main/scala/com/tort/mudai/person/Person.scala
@@ -5,22 +5,30 @@ import com.tort.mudai.event.{PasswordPromptEvent, LoginPromptEvent}
 import com.tort.mudai.command.SimpleCommand
 
 class Person(login: String, password: String) extends Actor {
-
   import context._
 
   val adapter = actorOf(Props[Adapter])
+  val snoopable = actorOf(Props[Snoopable])
 
   def receive = {
-    case Snoop(onRawRead) =>
-      become {
-        case rawRead: RawRead => onRawRead(rawRead.text)
-        case StopSnoop => unbecome
-        case rawWrite: RawWrite => adapter ! rawWrite
-        case Login => adapter ! Login
-        case Zap => adapter ! Zap
-        case e: LoginPromptEvent => adapter ! new SimpleCommand(login)
-        case e: PasswordPromptEvent => adapter ! new SimpleCommand(password)
-      }
+    case snoop: Snoop => snoopable ! snoop
+    case rawRead: RawRead => snoopable ! rawRead
+    case rawWrite: RawWrite => adapter ! rawWrite
+    case Login => adapter ! Login
+    case Zap => adapter ! Zap
+    case e: LoginPromptEvent => adapter ! new SimpleCommand(login)
+    case e: PasswordPromptEvent => adapter ! new SimpleCommand(password)
+  }
+}
+
+class Snoopable extends Actor {
+  import context._
+
+  def receive = {
+    case Snoop(onRawRead) => become {
+      case rawRead: RawRead => onRawRead(rawRead.text)
+      case StopSnoop => unbecome
+    }
   }
 }
 
