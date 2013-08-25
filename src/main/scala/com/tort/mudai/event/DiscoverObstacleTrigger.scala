e62614e030f278a6e1ad02eef5143bfbd395b707
tort <langeran@gmail.com>
2013-08-25 21:33:20 +0400

    add named obstacles to obstacles trigger

--- src/main/scala/com/tort/mudai/event/DiscoverObstacleTrigger.scala
+++ src/main/scala/com/tort/mudai/event/DiscoverObstacleTrigger.scala
@@ -1,13 +1,17 @@
 package com.tort.mudai.event
 
 class DiscoverObstacleTrigger extends EventTrigger[DiscoverObstacleEvent] {
-  val Pattern = ("(?ms).*(?:Закрыто\\.).*").r
+  val Pattern = ("(?ms).*Закрыто[^\n]*\\..*").r
+  val NamedObstaclePattern = ("(?ms).*Закрыто \\(([^\n]*)\\)\\..*").r
 
   override def matches(text: String) = {
     text.matches(Pattern.toString())
   }
 
   def fireEvent(text: String) = {
-    DiscoverObstacleEvent("дверь")
+    text match {
+      case NamedObstaclePattern(obstacle) => DiscoverObstacleEvent(obstacle)
+      case _ => DiscoverObstacleEvent("дверь")
+    }
   }
 }
