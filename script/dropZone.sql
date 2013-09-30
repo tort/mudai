f8755edf49164e69a3a1d1403320ba3de362d970
tort <langeran@gmail.com>
2013-09-30 21:09:22 +0400

    drop zone script update

--- script/dropZone.sql
+++ script/dropZone.sql
@@ -1,15 +1,8 @@
-delete from disposition where id in (
-select distinct h.id from disposition h join location l on h.location = l.id join zone z on l.zone = z.id join item m on h.item = m.id
-where z.name = 'На реке');
-
-delete from habitation where id in (
-select distinct h.id from habitation h join location l on h.location = l.id join zone z on l.zone = z.id join mob m on h.mob = m.id
-where z.name = 'На реке');
-
-delete from transition where id in (
-select distinct t.id from transition t join location l on (t.locFrom = l.id or t.locTo = l.id) join zone z on l.zone = z.id
-where z.name = 'На реке');
-
 delete from location where id in (
 select distinct l.id from location l join zone z on l.zone = z.id
 where z.name = 'На реке');
+
+delete from transition where locFrom not in (select id from location) or locTo not in(select id from location);
+delete from habitation t where t.location not in (select id from location);
+delete from disposition t where t.location not in (select id from location);
+
