505d9feed1ebfe4d8eead67df57bd9720ecec291
tort <langeran@gmail.com>
2013-10-12 22:23:59 +0400

    kill trigger fix. uncolored case, when mob kills

--- src/main/scala/com/tort/mudai/event/KillTrigger.scala
+++ src/main/scala/com/tort/mudai/event/KillTrigger.scala
@@ -1,8 +1,8 @@
 package com.tort.mudai.event
 
 class KillTrigger extends EventTrigger[KillEvent] {
-  val Pattern = ("""(?ms).*\u001B\[\d\;\dm([^\n]*) (?:мертв|мертва|мертвы), (?:его|ее) душа медленно подымается в небеса\..*""" +
-            """Ваш опыт повысился на ([1234567890]*) (?:очка|очков?)\.\r?\n.*"""
+  val Pattern = ("""(?ms).*(?:\u001B\[\d\;\dm)?([^\n]*) (?:мертв|мертва|мертвы), (?:его|ее) душа медленно подымается в небеса\..*""" +
+            """Ваш опыт повысился на ([1234567890]*) (?:очка|очков?)\..*"""
     ).r
 
   def matches(text: String) = text.matches(Pattern.toString)
