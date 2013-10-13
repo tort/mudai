c9f044db9d735ade279ee79c92445a321a33809a
tort <langeran@gmail.com>
2013-10-13 05:41:31 +0400

    map more zones

--- src/main/scala/com/tort/mudai/person/Passages.scala
+++ src/main/scala/com/tort/mudai/person/Passages.scala
@@ -24,5 +24,15 @@ class Passages(persister: LocationPersister, person: ActorRef) extends Actor {
       person ! MoveEvent(persister.loadLocation("ebb1d973-693f-4d3c-95f9-8b0f187f7eaa").some,
         direction.some,
         persister.loadLocation("a2487caf-444f-4736-978f-0f1fbbd6083d"))
+    case TriggeredMoveRequest("Дорога", direction, "Дорога") if direction == "trigger_swamp_south" =>
+      sender ! new SimpleCommand(s"дать ${level * 2} кун болотник")
+      person ! MoveEvent(persister.loadLocation("f6c1f708-c24c-41cc-b653-9ff36a64e731").some,
+        direction.some,
+        persister.loadLocation("9e0df04f-4bf2-4313-b305-fa84d574300b"))
+    case TriggeredMoveRequest("Дорога", direction, "Дорога") if direction == "trigger_swamp_north" =>
+      sender ! new SimpleCommand(s"дать ${level} кун болотник")
+      person ! MoveEvent(persister.loadLocation("2b7f6585-69eb-4e9f-8d46-bb649b42ca36").some,
+        direction.some,
+        persister.loadLocation("fcb3a6b8-0393-423e-aae6-7c2335e2c3bc"))
   }
 }
