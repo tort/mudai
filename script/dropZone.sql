c8fe1712469b523aceffc940f65a1822b152cb3e
tort <langeran@gmail.com>
2013-10-17 23:12:38 +0400

    update db

--- script/dropZone.sql
+++ script/dropZone.sql
@@ -1,8 +1,13 @@
+delete from transition where locfrom in (select distinct l.id from location l join zone z on l.zone = z.id
+where z.name = 'Сказка') or locto in (select distinct l.id from location l join zone z on l.zone = z.id
+where z.name = 'Сказка');
+
+delete from habitation where location in (select distinct l.id from location l join zone z on l.zone = z.id
+where z.name = 'Сказка');
+
+delete from disposition where location in (select distinct l.id from location l join zone z on l.zone = z.id
+where z.name = 'Сказка');
+
 delete from location where id in (
 select distinct l.id from location l join zone z on l.zone = z.id
-where z.name = 'На реке');
-
-delete from transition where locFrom not in (select id from location) or locTo not in(select id from location);
-delete from habitation t where t.location not in (select id from location);
-delete from disposition t where t.location not in (select id from location);
-
+where z.name = 'Сказка');
--- src/main/scala/com/tort/mudai/event/DisarmAssistantTrigger.scala
+++ src/main/scala/com/tort/mudai/event/DisarmAssistantTrigger.scala
@@ -1,12 +1,14 @@
 package com.tort.mudai.event
 
 class DisarmAssistantTrigger extends EventTrigger[DisarmAssistantEvent] {
-  val Pattern = """(?ms).*(?:\u001B\[\d\;\d\d?m)([^\n]*) ловко выбил ([^\n]*) из рук ([^\n]*)\..*""".r
+  val Pattern = """(?ms).*\n(?:\u001B\[\d\;\d\d?m)?([^\n]*) ловко выбил ([^\n]*) из рук ([^\n]*)\..*""".r
 
   def matches(text: String) = text.matches(Pattern.toString)
 
   def fireEvent(text: String) = {
     val Pattern(target, weapon, assistant) = text
+
+    println("FIRE DISARM ASSISTANT")
 
     DisarmAssistantEvent(target, weapon, assistant)
   }
